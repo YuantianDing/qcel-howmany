fd0e88be24dff606c41a376e51a6af182ee5e483
Yuantian Ding <dnailzb@outlook.com>
2025-09-03 19:58:08 +0800

    Updated Bin.

--- src/bin/test_quartz.rs
+++ src/bin/test_quartz.rs
@@ -3,22 +3,22 @@ use quclif::{circ::{gates::{CX, H, T, TDG, X, Z}, Instr}, instr_vec, search::{do
 
 
 fn main() {
-    let evaluator = Evaluator::from_random(5, &mut rand::rng());
+    let evaluator = Evaluator::from_random(3, &mut rand::rng());
     let eccs = CircuitECCs::generate(&evaluator, vec![*H, *X, *TDG, *T, *CX], 5);
     
     let instrs = instr_vec![
-        TDG 1;
+        H 0;
         CX 0,1;
-        T 1;
-        CX 0,1;
-        T 1;
+        H 0;
+        CX 1,0;
+        H 0;
     ];
     
     for i in 0..instrs.len() {
         for j in (i+1)..=instrs.len() {
             let (backstate, front_perm, back_perm) = evaluator.evaluate(&instrs[i..j]);
+            println!("{i}..{j} {front_perm} {} {back_perm}", instrs[i..j].iter().join_option(" ", "", ""));
             eccs.get(&backstate.hash_value()).map(|ecc| {
-                println!("{i}..{j} {front_perm} {} {back_perm}", instrs[i..j].iter().join_option(" ", "", ""));
                 for c in ecc.circuits.iter() {
                     println!("\t{}", c);
                 }
@@ -40,4 +40,6 @@ fn main() {
             eprintln!("\t{}", c.iter().join_option(" ", "", ""))
         }
     }
+
+    eccs.dump_quartz("eccset.json".into());
 }
